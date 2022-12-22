61fb2f342e84c7e41d83349f604499e8033c5d3d
Silver Okonkwo <silvernnaokonkwo@gmail.com>
2022-12-22 22:52:14 +0100

    next config

--- next.config.js
+++ next.config.js
@@ -4,13 +4,16 @@ const nextConfig = {
     formats: ['image/avif', 'image/webp'],
     domains: ["res.cloudinary.com", "s.gravatar.com", "lh3.googleusercontent.com"],
   },
+  Host: {
+    "CORS": "*"
+  },
   async headers() {
     return [
     {
       source: "/api/(.*)",
       headers: [
      { key: "Access-Control-Allow-Credentials", value: "true" },
-     { key: "Access-Control-Allow-Origin", value: "https://pizza-ordering-8f1q08h0h-mamabear25.vercel.app/api/promos" },
+     { key: "Access-Control-Allow-Origin", value: "https://pizza-ordering-i3166npmf-mamabear25.vercel.app" },
      { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
      { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
     ]
