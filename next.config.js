3ada895f21d5e23a834c4dc12e5748d7c4af2370
Silver Okonkwo <silvernnaokonkwo@gmail.com>
2022-12-22 22:58:56 +0100

    next

--- next.config.js
+++ next.config.js
@@ -1,25 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+      { key: "Access-Control-Allow-Credentials", value: "true" },
+      { key: "Access-Control-Allow-Origin", value: "*" },
+      { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+      { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
   images: {
     formats: ['image/avif', 'image/webp'],
     domains: ["res.cloudinary.com", "s.gravatar.com", "lh3.googleusercontent.com"],
-  },
-  Host: {
-    "CORS": "*"
-  },
-  async headers() {
-    return [
-    {
-      source: "/api/(.*)",
-      headers: [
-     { key: "Access-Control-Allow-Credentials", value: "true" },
-     { key: "Access-Control-Allow-Origin", value: "https://pizza-ordering-i3166npmf-mamabear25.vercel.app" },
-     { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
-    ]
-    }
-    ]
-  },
-}
+  }
+};
 
 module.exports = nextConfig
