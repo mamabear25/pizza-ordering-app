49fb4eb56bd8df8b0d61c600665ae6be35b3ce5d
Silver Okonkwo <silvernnaokonkwo@gmail.com>
2022-12-23 17:13:15 +0100

    cors fixed

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
       { key: "Access-Control-Allow-Credentials", value: "true" },
-      { key: "Access-Control-Allow-Origin", value: "https://pizza-ordering-ee6e6cq0m-mamabear25.vercel.app/" },
+      { key: "Access-Control-Allow-Origin", value: "https://pizza-ordering-esfmu1v0f-mamabear25.vercel.app" },
       { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
       { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
