b4d531979923f6a13524fb992c4d7f4c874320c4
Silver Okonkwo <silvernnaokonkwo@gmail.com>
2022-12-27 19:20:34 +0100

    cors

--- pages/api/promos/[id].js
+++ pages/api/promos/[id].js
@@ -1,15 +1,40 @@
 import dbConnect from "../../../util/mongo";
 import Promo from "../../../models/Promo";
+import Cors from 'cors';
+
+// Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+const cors = Cors({
+    methods: ['POST', 'GET', 'HEAD', 'OPTIONS'],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+    return new Promise((resolve, reject) => {
+      fn(req, res, result => {
+        if (result instanceof Error) {
+          return reject(result);
+        }
+  
+        return resolve(result);
+      });
+    });
+}
 
 export default async function handler(req, res) {
+
+    await runMiddleware(req, res, cors)
+
     const { 
         method, 
         query: { id },
     } = req;
 
     if (method === 'OPTIONS') {
-        res.status(200).send("ok");
-        return;
+        return res.status(200).json(({
+        body: "OK"
+        }))
     }
 
     await dbConnect()
