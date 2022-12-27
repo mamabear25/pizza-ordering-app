ed6c4461d9ac187f03fdc1e0a1fcd76a65767150
Silver Okonkwo <silvernnaokonkwo@gmail.com>
2022-12-27 18:37:49 +0100

    beworple

--- components/PromoList.jsx
+++ components/PromoList.jsx
@@ -7,7 +7,14 @@ const PromoList = () => {
     const [promoList, setPromoList] = useState([]);
 
     const getPromoList = async () => {
-    const res = await axios.get("https://pizza-ordering-8f1q08h0h-mamabear25.vercel.app/api/promos");
+    const res = await axios.get("https://pizza-ordering-8f1q08h0h-mamabear25.vercel.app/api/promos", {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          "Access-Control-Allow-Headers": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" ,
+          "Access-Max-Age": "86400"
+        }
+      });
 
     setPromoList(res.data);
     };
