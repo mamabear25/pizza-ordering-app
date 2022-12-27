b8c01bd8ecda7f8ca688af2546f9f2f348829a61
Silver Okonkwo <silvernnaokonkwo@gmail.com>
2022-12-27 18:34:04 +0100

    whew

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export const getServerSideProps = async () => {
   // vercel
   const res = await axios.get("https://pizza-ordering-8f1q08h0h-mamabear25.vercel.app/api/promos", {
     headers: {
-      "Access-Control-Allow-Origin": "https://pizza-ordering-8f1q08h0h-mamabear25.vercel.app",
+      "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
       "Access-Control-Allow-Headers": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" ,
       "Access-Max-Age": "86400"
