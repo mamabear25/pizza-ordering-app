2d34bffbaf02e86d04acf2675c0400cf65b4b78e
Silver Okonkwo <silvernnaokonkwo@gmail.com>
2022-12-28 11:06:40 +0100

    updated auth0 credentials

--- pages/logout.jsx
+++ pages/logout.jsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
 
 export default function Logout() {
-    return <Link href="/api/auth/logout">Logout</Link>;
+    return <Link href="https://pizza-jb1hwqmhw-mamabear25.vercel.app/api/auth/logout">Logout</Link>;
 }
